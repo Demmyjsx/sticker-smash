d1aef82d36f01b29d41027b3ea053909edef6bb5
Demmyjsx <ademolaabds@gmail.com>
2025-06-28 12:26:43 +0100

    t's an app named StickerSmash that runs on Android, iOS, and the web:

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
 
 
+const PlaceHolderImage = require("../../assets/images/background-image.png")
 
 export default function Index() {
   return (
     <View
    style={ styles.container}
     >
+      <Image source={PlaceHolderImage} style={styles.image} /> 
       <Text style={styles.text}>Home Screen</Text>
      
       
@@ -30,5 +33,10 @@ button:{
   fontSize:20,
   textDecorationLine: "underline",
   color: "white",
-}
+},
+image: {
+  width: 340,
+  height: 440,
+  borderRadius: 18,
+},
 })
